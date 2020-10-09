48c30fb04a3a4d136a79bf61a1d7195f2a3ab113
hendra0ma <hendra0maulidan@gmail.com>
2020-10-09 21:23:27 +0700

    membuat fungsi isLength dan manambah readme

--- style.js
+++ style.js
@@ -1,27 +1,26 @@
-// const button = document.getElementById('btnButton');
-        
-//         const firstName = document.getElementById('firstName');     // ini untuk mengambil firstName
-//         const lastName = document.getElementById('lastName');     
-//         const city = document.getElementById('city');     
-//         const zipCode = document.getElementById('zipCode');     
-//         const check = document.getElementById('check');     
-        // button.addEventListener('click',function () {
-        // console.log(getFormData())
-        // })
+        const button = document.getElementById('btnButton');
+        const firstName = document.getElementById('firstName');     // ini untuk mengambil firstName
+        const lastName = document.getElementById('lastName');     
+        const city = document.getElementById('city');     
+        const zipCode = document.getElementById('zipCode');     
+        const check = document.getElementById('check');     
+        button.addEventListener('click',function () {
+        console.log(getFormData())
+        })
 
 
-        // function getFormData() {
-        //     var ambilNama = {
-        //         firstName : firstName.value,
-        //         lastName : lastName.value,
-        //         city : city.value,
-        //         zipCode : zipCode.value,
-        //         check : check.value,
-        //     }
-        //     return ambilNama;
-        // }
+        function getFormData() {
+            var ambilNama = {
+                firstName : firstName.value,
+                lastName : lastName.value,
+                city : city.value,
+                zipCode : zipCode.value,
+                check : check.value,
+            }
+            return ambilNama;
+        }
 
-        let angka = "s";
+        let angka = "sayaHendra";
         
         let isNumber = (string) => {
             if(typeof string == "number"){
@@ -33,21 +32,22 @@
         console.log( "tipe data dari variable angka adalah "+
                         typeof angka+
                          " jadi nilai dari angka adalah "+
-                        isNumber(angka))
+                        isNumber(angka));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        let isLength = (string, integer) => {
+            if (typeof integer == "number") {
+                if(string.length == integer){
+                    return true;
+                }else{
+                    return false;
+                }    
+            }else{
+                console.log("gagal");
+            }
+        }
+        console.log(isLength("botol",5));
 
 
 
