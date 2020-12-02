9430a35737fa3fb8f1f1b6b05675801bf4747262
hendra0ma <hendra0maulidan@gmail.com>
2020-12-02 15:37:07 +0700

    Section Main

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+let teksHead = document.getElementById("teks");
 let h1 = document.querySelector("nav ul li h1");
 
 setInterval(function() {
@@ -5,3 +6,11 @@ setInterval(function() {
   h1.innerHTML =
     waktu.getHours() + " : " + waktu.getMinutes() + " : " + waktu.getSeconds();
 }, 1000);
+
+setInterval(function() {
+  if (teksHead.innerHTML == "Join Us To Find <b> Another Stuff</b>") {
+    teksHead.innerHTML = "Follow Us For The<b> Best Experience</b>";
+  } else {
+    teksHead.innerHTML = "Join Us To Find <b> Another Stuff</b>";
+  }
+}, 2000);
